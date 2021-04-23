8e8bc0fbd3cb6c68b74cbef1054180ce733edb57
tymonomyt <tymoteusz1138@gmail.com>
2021-04-23 11:12:04 +0200

    change IDs to strings

---
+++ CoinAPI/Migrations/20210423090905_fix.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoinAPI.Migrations
+{
+    public partial class fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Currencies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "temp",
+                table: "Currencies",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies",
+                column: "temp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies");
+
+            migrationBuilder.DropColumn(
+                name: "temp",
+                table: "Currencies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Currencies",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies",
+                column: "Code");
+        }
+    }
+}
---
+++ CoinAPI/Migrations/20210423091013_fix2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoinAPI.Migrations
+{
+    public partial class fix2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Currencies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Currencies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
